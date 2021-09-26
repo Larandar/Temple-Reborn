82e1c13458f61f3bca3b3941cc87f6bfa9211bcb
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2021-09-26 18:38:38 +0200

    ✨ (core): Complete core settings

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,30 @@ import {
     App,
     Plugin,
     PluginSettingTab,
-    Setting
+    SearchComponent,
+    Setting,
+    TextComponent
 } from "obsidian";
+import { FolderSuggest } from "suggest";
 
 interface TempleCoreSettings {
-    templateDirectory: string;
+    templateDirectory: string
+    filterTemplateSelect: {
+        enable: boolean
+        regex: string
+    }
 }
 
 const DEFAULT_SETTINGS: TempleCoreSettings = {
     templateDirectory: "_templates",
+    filterTemplateSelect: {
+        enable: true,
+        regex: "^_",
+    }
 };
 
 export default class TempleRebornPlugin extends Plugin {
-    settings: TempleCoreSettings;
+    settings: TempleCoreSettings
 
     async onload() {
         // Laod plugin settings
@@ -62,14 +73,27 @@ class TempleSettingTab extends PluginSettingTab {
         new Setting(containerEl)
             .setName("Template folder location")
             .setDesc("Files in this directory will be available as templates.")
-            .addSearch(search => {
-                // TODO: add suggestion
+            .addSearch((search: SearchComponent) => {
+                search.setValue(this.plugin.settings.templateDirectory)
                 search.onChange(async value => {
                     this.plugin.settings.templateDirectory = value;
                     await this.plugin.saveSettings();
                 })
+
+                new FolderSuggest(this.app, search.inputEl)
+            })
+
+        // core.filterTemplateSelect
+        // TODO: Add a toggle switch in the same line for filterTemplateSelect.enable
+        new Setting(containerEl)
+            .setName("Files to exclude from fuzzy search")
+            .setDesc("Files matching the regex will be ignored (Default: any file starting by `_`).")
+            .addText((text: TextComponent) => {
+                text.setValue(this.plugin.settings.filterTemplateSelect.regex)
+                text.onChange(async value => {
+                    this.plugin.settings.filterTemplateSelect.regex = value;
+                    await this.plugin.saveSettings();
+                })
             })
     }
-
-
 }
